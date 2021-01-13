4e186814f017c9c67de72eec99ab32387aaeebeb
Paweł Tomczyk <pawerk4pl@gmail.com>
2021-01-13 20:07:38 +0100

    PHP-38 fixed navbar responsivness and looks

--- resources/views/components/navbar/bar.blade.php
+++ resources/views/components/navbar/bar.blade.php
@@ -1,4 +1,4 @@
-@props(['logo','names','icons','destinations'])
+@props(['names','icons','destinations'])
 
 @php
     $icons=explode(' ',$icons);
@@ -7,31 +7,33 @@
     $size=min(count($destinations),count($icons),count($names));
 @endphp
 
-<nav class="bg-white border-b border-gray-100">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
 
-                {{--LOGO--}}
-                <div class="flex-shrink-0 flex ">
-                    <a class="inline-flex items-center px-1 pt-1 text-3xl font-medium leading-5 text-blue-500">{{$logo}}</a>
-                </div>
+<nav class="bg-white flex justify-between h-10 md:h-12 lg:h-14 xl:h-16 ">
 
-                {{--LINKS--}}
-                <div class="hidden space-x-8 sm:-my-px px-20 sm:ml-10 sm:flex">
-
-                    @for( $i=0;$i<$size;$i++)
-                        <x-navbar.link :destination="(empty( $destinations[$i] ))?'events.index':$destinations[$i]"
-                                       :icon="(empty( $icons[$i] ))?'view-list':$icons[$i]" >
-                            {{ __(empty($names[$i]))?'Link':$names[$i] }}
-                        </x-navbar.link>
-                    @endfor
-
-                </div>
-            </div>
-
-            {{--DROPDOWNMENU--}}
-
-        </div>
+    {{--LOGO--}}
+    <div class="flex items-center pl-1 sm:pl-5">
+        <a class="text-lg md:text-xl lg:text-2xl xl:text-3xl text-blue-500">{{$slot}}</a>
     </div>
+
+    {{--LINKS--}}
+    <div class="flex  justify-center ">
+
+        @for( $i=0;$i<$size;$i++)
+            <x-navbar.link :destination="(empty( $destinations[$i] ))?'events.index':$destinations[$i]"
+                           :icon="(empty( $icons[$i] ))?'view-list':$icons[$i]"
+                           class="flex items-center px-1 sm:px-2 md:px-3 md:px-4 lg:px-5 text-sm md:text-base lg:text-md xl:text-lg">
+                {{ __($names[$i]) }}
+            </x-navbar.link>
+        @endfor
+
+    </div>
+
+    {{--DROPDOWNMENU--}}
+    <div class="flex items-center pr-1 sm:pr-5">
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <a href="route('logout')" onclick="event.preventDefault();this.closest('form').submit();">Logout</a>
+        </form>
+    </div>
+
 </nav>
