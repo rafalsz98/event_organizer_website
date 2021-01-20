b65b35a6e96e9d855bc7d9af49f62ef7bde2ebe3
Rafal Szczepanik <rafal.sz@tutanota.com>
2021-01-20 18:18:34 +0100

    PHP-18 Created dashboard page, hot fixes to navbar and event tile. Added event seeder

--- resources/views/debug/test-eventTab-eventTile.blade.php
+++ resources/views/debug/test-eventTab-eventTile.blade.php
@@ -2,7 +2,7 @@
     @php
         $event = new \App\Models\Event();
         $event->id = 1;
-        $event->datestart = new DateTime('15.01.2021 15:30');
+        $event->datestart = '15.01.2021 15:30';
         $event->duration = 2;
         $event->name = "Power Rangers";
         $event->current_participants=27;
